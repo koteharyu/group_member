fb3e5d100b0fca9cd06bcbbac7c7be3e23166dcf
haryu <hryau6@gmail.com>
2021-08-20 14:04:15 +0900

    add: グループからの脱退機能の追加

--- app/controllers/operate_members/participates_controller.rb
+++ app/controllers/operate_members/participates_controller.rb
@@ -8,6 +8,13 @@ class OperateMembers::ParticipatesController < ApplicationController
     redirect_to group_path(@group)
   end
 
+  def destroy
+    new_member_ids = []
+    new_member_ids = @group.member_ids.filter{ |member| member != params[:member_id].to_i }
+    @group.member_ids = new_member_ids
+    redirect_to group_path(@group)
+  end
+
   private
 
   def set_group
