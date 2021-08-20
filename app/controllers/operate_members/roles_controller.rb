a5c6a69674c1217ee3e4a2cb982f8d3fab3212ac
haryu <hryau6@gmail.com>
2021-08-20 14:22:44 +0900

    add: roles/updateの処理内にもグループにメンバーがいない場合の処理を追加

--- app/controllers/operate_members/roles_controller.rb
+++ app/controllers/operate_members/roles_controller.rb
@@ -2,6 +2,11 @@ class OperateMembers::RolesController < ApplicationController
   before_action :set_group
 
   def update
+    if @group.members.blank?
+      redirect_to group_path(@group), notice: 'メンバーを追加してください'
+      return
+    end
+
     current_organizer = @group.group_members&.find_by(role: 1)
     if current_organizer.nil?
       target_members = @group.group_members.all
@@ -10,8 +15,8 @@ class OperateMembers::RolesController < ApplicationController
     else
       target_members = @group.group_members.filter{ |m| m.id != current_organizer.id  }
       selected_member = target_members.sample
-      selected_member.update(role: 1)
-      current_organizer.update(role: 0)
+      selected_member&.update(role: 1)
+      current_organizer&.update(role: 0)
     end
     redirect_to group_path(@group), notice: 'Succeeded in setting the Organizer'
   end
