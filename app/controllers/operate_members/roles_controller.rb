e809e1840407070c3d122a0b5deb4e74e424fd49
haryu <hryau6@gmail.com>
2021-08-20 14:14:51 +0900

    add: 幹事の設定機能を追加

--- app/controllers/operate_members/roles_controller.rb
+++ app/controllers/operate_members/roles_controller.rb
@@ -1,2 +1,24 @@
 class OperateMembers::RolesController < ApplicationController
+  before_action :set_group
+
+  def update
+    current_organizer = @group.group_members&.find_by(role: 1)
+    if current_organizer.nil?
+      target_members = @group.group_members.all
+      selected_member = target_members.sample
+      selected_member&.update(role: 1)
+    else
+      target_members = @group.group_members.filter{ |m| m.id != current_organizer.id  }
+      selected_member = target_members.sample
+      selected_member.update(role: 1)
+      current_organizer.update(role: 0)
+    end
+    redirect_to group_path(@group), notice: 'Succeeded in setting the Organizer'
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
 end
