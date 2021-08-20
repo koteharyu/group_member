259a83e396cceb59e13b1b688fbd2b26a95cc9fa
haryu <hryau6@gmail.com>
2021-08-20 13:30:45 +0900

    add: operate_membersに関するルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :members
   resources :groups
+
+  namespace :operate_members do
+    resources :groups, only: %i[update destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
